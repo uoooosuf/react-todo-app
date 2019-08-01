ed1bdd307141283c2a88796dfc3f426f5c99f5a0
Yusuf Abubakar <uoooosuf@email.com>
2019-08-01 02:27:37 +0200

    started my react todo app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+<div class="app">
+  <div class="todo-container">
+    <div class="todo-item-row">Learn React</div>
+    <div class="todo-item-row">Learn Redux</div>
+    <div class="todo-item-row">Learn React Native</div>
+  </div>
+</div>
+*/
+
+class App extends Component {
+  render() {
+    return React.createElement(
+      "div",
+      {
+        className: "app"
+      },
+      React.createElement(
+        "div",
+        {
+          className: "todo-container"
+        },
+        React.createElement(
+          "div",
+          {
+            className: "todo-item-row"
+          },
+          "Learn React"
+        ),
+        React.createElement(
+          "div",
+          {
+            className: "todo-item-row"
+          },
+          "Learn Redux"
+        ),
+        React.createElement(
+          "div",
+          {
+            className: "todo-item-row"
+          },
+          "Learn React Native"
+        )
+      )
+    );
+  }
 }
 
 export default App;
