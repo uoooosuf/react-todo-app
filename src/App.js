b40e1f79111b9b1b644f9174ee3e3a1931faac80
Yusuf Abubakar <uoooosuf@email.com>
2019-08-02 06:37:03 +0200

    added semantic-ui style and delete function

--- src/App.js
+++ src/App.js
@@ -1,51 +1,168 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Table, Checkbox, Button } from "semantic-ui-react";
+
 import "./App.css";
 
-/*
-<div class="app">
-  <div class="todo-container">
-    <div class="todo-item-row">Learn React</div>
-    <div class="todo-item-row">Learn Redux</div>
-    <div class="todo-item-row">Learn React Native</div>
-  </div>
-</div>
-*/
+const TodoItem = props => (
+  <Table.Row>
+    <Table.Cell>
+      <Checkbox />
+    </Table.Cell>
+    <Table.Cell>
+      {props.children}
+      <Button color="red" icon="trash" floated="right" compact size="small" />
+    </Table.Cell>
+  </Table.Row>
+);
 
 class App extends Component {
-  render() {
-    return React.createElement(
-      "div",
+  state = {
+    todos: [
+      { title: "Learn React", completed: false },
+      { title: "Learn Redux", completed: false },
+      { title: "Learn React Native", completed: false },
       {
-        className: "app"
-      },
-      React.createElement(
-        "div",
-        {
-          className: "todo-container"
-        },
-        React.createElement(
-          "div",
-          {
-            className: "todo-item-row"
-          },
-          "Learn React"
-        ),
-        React.createElement(
-          "div",
-          {
-            className: "todo-item-row"
-          },
-          "Learn Redux"
-        ),
-        React.createElement(
-          "div",
-          {
-            className: "todo-item-row"
-          },
-          "Learn React Native"
-        )
-      )
+        title: "Create a brand new web app!",
+        completed: false
+      }
+    ],
+    newTodo: ""
+  };
+
+  handleToggleAll = () => {
+    const [...todos] = this.state.todos;
+    const allToggled = todos.every(todo => todo.completed);
+    const toggledTodos = todos.map(todo => ({
+      ...todo,
+      completed: !allToggled
+    }));
+
+    this.setState({ todos: toggledTodos });
+  };
+
+  handleTodoClick(todo, index) {
+    const { completed } = todo;
+    const [...todos] = this.state.todos;
+    todos[index] = {
+      ...todo,
+      completed: !completed
+    };
+    this.setState({ todos });
+  }
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    this.setState({ newTodo: value });
+  };
+
+  handleNewTodoKeyDown = event => {
+    if (this.state.todos.length >= 10) {
+      // don't allow more than 10 todos
+      return;
+    }
+
+    if (event.keyCode !== 13) {
+      // 13 is enter key
+      return;
+    }
+    event.preventDefault();
+
+    const { newTodo, todos } = this.state;
+    const value = newTodo.trim();
+    if (value) {
+      this.setState({
+        todos: [...todos, { title: value, completed: false }],
+        newTodo: ""
+      });
+    }
+  };
+
+  handleDelete = (todo, i) => {
+    const { todos } = this.state;
+    const todosWithoutDeletedTodo = todos.filter((t, index) => index !== i);
+    this.setState({ todos: todosWithoutDeletedTodo });
+  };
+
+  handleClearCompleted = () => {
+    const { todos } = this.state;
+    const incompleteTodos = todos.filter(todo => !todo.completed);
+    this.setState({ todos: incompleteTodos });
+  };
+
+  render() {
+    const { todos, newTodo } = this.state;
+    const allToggled = todos.every(todo => todo.completed);
+    return (
+      <div className="app">
+        <div className="todo-container">
+          <input
+            id="new-todo"
+            className="new-todo"
+            placeholder="What needs to be done?"
+            autoFocus
+            value={this.state.newTodo}
+            onChange={this.handleInputChange}
+            onKeyDown={this.handleNewTodoKeyDown}
+          />
+          <label htmlFor="new-todo" style={{ display: "none" }}>
+            New Todo
+          </label>
+          {todos.length === 0 ? (
+            <Table>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>You have nothing to do!</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+            </Table>
+          ) : (
+            <Table>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>
+                    <Checkbox
+                      checked={allToggled}
+                      onChange={this.handleToggleAll}
+                    />
+                  </Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {this.state.todos.map((todo, i) => (
+                  <Table.Row key={i} positive={todo.completed}>
+                    <Table.Cell>
+                      <Checkbox
+                        checked={todo.completed}
+                        onChange={() => this.handleTodoClick(todo, i)}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      {todo.title}
+                      <Button
+                        color="red"
+                        icon="trash"
+                        floated="right"
+                        compact
+                        size="small"
+                        onClick={() => this.handleDelete(todo, i)}
+                      />
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+              <Table.Footer fullWidth>
+                <Table.Row>
+                  <Table.HeaderCell colSpan="2">
+                    <Button size="small" onClick={this.handleClearCompleted}>
+                      Clear Completed
+                    </Button>
+                  </Table.HeaderCell>
+                </Table.Row>
+              </Table.Footer>
+            </Table>
+          )}
+        </div>
+      </div>
     );
   }
 }
